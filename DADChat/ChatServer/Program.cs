404ddc51c5ae05744fd8985e9b69ce069c368acd
0x000001A4 <ricardo.m.rocha@protonmail.com>
2022-10-08 12:56:50 +0100

    Setting up perfect channels: Queue of messages when process is frozen

--- DADChat/ChatServer/Program.cs
+++ DADChat/ChatServer/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Grpc.Core.Interceptors;
 
 namespace ChatServer {
 	// ChatServerService is the namespace defined in the protobuf
@@ -11,9 +14,10 @@ namespace ChatServer {
 
 		private Dictionary<string, string> clientMap = new Dictionary<string, string>();
 		private ClientFrontend clientFrontend = new ClientFrontend();
+		private Program process;
 
-		public ServerService() {
-
+		public ServerService(Program _process) {
+			process = _process;
 		}
 
 		public override Task<ChatClientRegisterReply> Register(
@@ -22,12 +26,17 @@ namespace ChatServer {
 		}
 		
 		public ChatClientRegisterReply Reg(ChatClientRegisterRequest request) {
-			lock (this) {
-				clientMap.Add(request.Nick, request.Url);
-			}
-			Console.WriteLine($"Registered client {request.Nick} with URL {request.Url}");
+            if (process.Frozen) return new ChatClientRegisterReply { Ok = true };
+			doReg(request);
 			return new ChatClientRegisterReply { Ok = true };
 		}
+
+		void doReg(ChatClientRegisterRequest request) {
+            lock (this) {
+                clientMap.Add(request.Nick, request.Url);
+            }
+            Console.WriteLine($"Registered client {request.Nick} with URL {request.Url}");
+        }
 
 		public override Task<ChatClientSendMessageReply> SendMessage(
 			ChatClientSendMessageRequest request, ServerCallContext context) {
@@ -36,24 +45,78 @@ namespace ChatServer {
 		}
 
 		public ChatClientSendMessageReply HandleMsg(ChatClientSendMessageRequest request) {
-			clientFrontend.BroadcastMsg(request.Msg, clientMap, request.From);
-			Console.WriteLine($"Received Message: {request.Msg}");
+			if (process.Frozen) return new ChatClientSendMessageReply { Ok = true };
+			doHandleMsg(request);
 			return new ChatClientSendMessageReply { Ok = true }; 
 		}
 
+		public void doHandleMsg(ChatClientSendMessageRequest request) {
+            clientFrontend.BroadcastMsg(request.Msg, clientMap, request.From);
+            Console.WriteLine($"Received Message: {request.Msg}");
+        }
+
 	}
-	class Program {
+
+	public class ServerMessageInterceptor : Interceptor {
+
+		public Program process;
+		public int i = 0;
+
+		public ServerMessageInterceptor(Program _process) {
+			process = _process;
+		}
+
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+			TRequest request,
+			ServerCallContext context,
+			UnaryServerMethod<TRequest, TResponse> continuation)
+		{
+            try {
+				i = i + 1;
+                if (process.Frozen && i > 0) {
+                    process.queue.Enqueue((ChatClientSendMessageRequest)(object)(request));
+                }
+                return await continuation(request, context);
+
+			} catch (Exception ex) {
+				throw ex;
+			}
+		}
+	}
+
+	public class Program {
 		const int Port = 8000;
-		static void Main(string[] args) {
-			Server server = new Server {
-				Services = { ChatServerService.BindService(new ServerService()) },
+		public bool Frozen { get; set; } = false;
+        public Queue<ChatClientSendMessageRequest> queue { get; set; } = new Queue<ChatClientSendMessageRequest>();
+
+		public void handleQueuedRequest(ServerService service, ChatClientSendMessageRequest request) {
+			service.doHandleMsg(request);
+		}
+
+        static void Main(string[] args) {
+			Program process = new Program();
+
+			ServerService _service = new ServerService(process);
+            Server server = new Server {
+				Services = { ChatServerService.BindService(_service).Intercept(new ServerMessageInterceptor(process)) },
 				Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
 			};
+
+			Console.WriteLine(process.Frozen);
 			server.Start();
 			Console.WriteLine("ChatServer server listening on port " + Port);
 			Console.WriteLine("Press any key to stop the server...");
-			Console.ReadKey();
 
+			ConsoleKey key;
+			while ((key = Console.ReadKey().Key) != ConsoleKey.Escape) {
+				if (key == ConsoleKey.F) {
+					process.Frozen = !process.Frozen;
+                    Console.WriteLine($"Command F: Process is now {(process.Frozen == true ? "frozen" : "active")}");
+					while (process.Frozen == false && process.queue.Count != 0 ) {
+						process.handleQueuedRequest(_service, process.queue.Dequeue());
+					}
+				}
+			}
 			server.ShutdownAsync().Wait();
 
 		}
